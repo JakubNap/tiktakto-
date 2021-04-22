5f89399553677fd6f4b676571ad5ded43919283c
JakubNap <82877122+JakubNap@users.noreply.github.com>
2021-04-22 21:51:51 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace tiktaktoł
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.BR = new System.Windows.Forms.Button();
+            this.BC = new System.Windows.Forms.Button();
+            this.BL = new System.Windows.Forms.Button();
+            this.CR = new System.Windows.Forms.Button();
+            this.CC = new System.Windows.Forms.Button();
+            this.CL = new System.Windows.Forms.Button();
+            this.TR = new System.Windows.Forms.Button();
+            this.T = new System.Windows.Forms.Button();
+            this.TL = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.currentPlayerLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.BR, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.BC, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.BL, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.CR, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.CC, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.CL, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.TR, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.T, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.TL, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(250, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(358, 360);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // BR
+            // 
+            this.BR.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BR.Location = new System.Drawing.Point(241, 243);
+            this.BR.Name = "BR";
+            this.BR.Size = new System.Drawing.Size(113, 114);
+            this.BR.TabIndex = 8;
+            this.BR.UseVisualStyleBackColor = true;
+            this.BR.Click += new System.EventHandler(this.Mark);
+            // 
+            // BC
+            // 
+            this.BC.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BC.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BC.Location = new System.Drawing.Point(122, 243);
+            this.BC.Name = "BC";
+            this.BC.Size = new System.Drawing.Size(113, 114);
+            this.BC.TabIndex = 7;
+            this.BC.UseVisualStyleBackColor = true;
+            this.BC.Click += new System.EventHandler(this.Mark);
+            // 
+            // BL
+            // 
+            this.BL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BL.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BL.Location = new System.Drawing.Point(3, 243);
+            this.BL.Name = "BL";
+            this.BL.Size = new System.Drawing.Size(113, 114);
+            this.BL.TabIndex = 6;
+            this.BL.UseVisualStyleBackColor = true;
+            this.BL.Click += new System.EventHandler(this.Mark);
+            // 
+            // CR
+            // 
+            this.CR.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CR.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CR.Location = new System.Drawing.Point(241, 123);
+            this.CR.Name = "CR";
+            this.CR.Size = new System.Drawing.Size(114, 114);
+            this.CR.TabIndex = 5;
+            this.CR.UseVisualStyleBackColor = true;
+            this.CR.Click += new System.EventHandler(this.Mark);
+            // 
+            // CC
+            // 
+            this.CC.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CC.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CC.Location = new System.Drawing.Point(122, 123);
+            this.CC.Name = "CC";
+            this.CC.Size = new System.Drawing.Size(113, 114);
+            this.CC.TabIndex = 4;
+            this.CC.UseVisualStyleBackColor = true;
+            this.CC.Click += new System.EventHandler(this.Mark);
+            // 
+            // CL
+            // 
+            this.CL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CL.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CL.Location = new System.Drawing.Point(3, 123);
+            this.CL.Name = "CL";
+            this.CL.Size = new System.Drawing.Size(113, 114);
+            this.CL.TabIndex = 3;
+            this.CL.UseVisualStyleBackColor = true;
+            this.CL.Click += new System.EventHandler(this.Mark);
+            // 
+            // TR
+            // 
+            this.TR.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TR.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TR.Location = new System.Drawing.Point(241, 3);
+            this.TR.Name = "TR";
+            this.TR.Size = new System.Drawing.Size(114, 114);
+            this.TR.TabIndex = 2;
+            this.TR.UseVisualStyleBackColor = true;
+            this.TR.Click += new System.EventHandler(this.Mark);
+            // 
+            // T
+            // 
+            this.T.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.T.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.T.Location = new System.Drawing.Point(122, 3);
+            this.T.Name = "T";
+            this.T.Size = new System.Drawing.Size(113, 114);
+            this.T.TabIndex = 1;
+            this.T.UseVisualStyleBackColor = true;
+            this.T.Click += new System.EventHandler(this.Mark);
+            // 
+            // TL
+            // 
+            this.TL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TL.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TL.Location = new System.Drawing.Point(3, 3);
+            this.TL.Name = "TL";
+            this.TL.Size = new System.Drawing.Size(113, 114);
+            this.TL.TabIndex = 0;
+            this.TL.UseVisualStyleBackColor = true;
+            this.TL.Click += new System.EventHandler(this.Mark);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 414);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Aktualny ruch:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // currentPlayerLabel
+            // 
+            this.currentPlayerLabel.AutoSize = true;
+            this.currentPlayerLabel.Location = new System.Drawing.Point(340, 414);
+            this.currentPlayerLabel.Name = "currentPlayerLabel";
+            this.currentPlayerLabel.Size = new System.Drawing.Size(0, 15);
+            this.currentPlayerLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 653);
+            this.Controls.Add(this.currentPlayerLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button TL;
+        private System.Windows.Forms.Button BR;
+        private System.Windows.Forms.Button BC;
+        private System.Windows.Forms.Button BL;
+        private System.Windows.Forms.Button CR;
+        private System.Windows.Forms.Button CC;
+        private System.Windows.Forms.Button CL;
+        private System.Windows.Forms.Button TR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button T;
+        private System.Windows.Forms.Label currentPlayerLabel;
+    }
+}
+
